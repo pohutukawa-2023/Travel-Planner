3269011504720e2733320a429618fc5ade6e1289
Will Yang <willyangzzz@gmail.com>
2023-11-28 18:41:47 +1300

    create homepage

---
+++ client/components/HomePage.tsx
@@ -0,0 +1,14 @@
+function HomePage() {
+  return (
+    <>
+      <h2>You will never travel without our trip planner again</h2>
+      <h3>
+        Build, organize, and map your itineraries in a free travel app designed
+        for vacations & road trips
+      </h3>
+      <button>Start Planning</button>
+      <button>Get the app</button>
+    </>
+  )
+}
+export default HomePage
