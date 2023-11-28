2dd6ccb6aeb99f1d28e8d5b7142b32842b6c26e5
Will Yang <willyangzzz@gmail.com>
2023-11-28 19:38:42 +1300

    create homepage css

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -1,13 +1,15 @@
 function HomePage() {
   return (
     <>
-      <h2>You will never travel without our trip planner again</h2>
-      <h3>
-        Build, organize, and map your itineraries in a free travel app designed
-        for vacations & road trips
-      </h3>
-      <button>Start Planning</button>
-      <button>Get the app</button>
+      <div className="homeContent">
+        <h2>You will never travel without our trip planner again</h2>
+        <h3>
+          Build, organize, and map your itineraries in a free travel app
+          designed for vacations & road trips
+        </h3>
+        <button className="btn start-btn">Start Planning</button>
+        <button className="btn app-btn">Get the app</button>
+      </div>
     </>
   )
 }
