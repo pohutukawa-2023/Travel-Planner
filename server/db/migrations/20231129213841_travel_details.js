e04f0f6b72a6e9c3c0f9c50a078d0bfcbae68e92
Ricky <65267289+ricky-huang-dev@users.noreply.github.com>
2023-11-30 10:59:06 +1300

    create migrate files for database

---
+++ server/db/migrations/20231129213841_travel_details.js
@@ -0,0 +1,13 @@
+export async function up(knex) {
+  await knex.schema.createTable('travel_details', (table) => {
+    table.increments('id').primary()
+    table.string('user_id').references('users.auth0_id')
+    table.integer('suggestion_id').references('suggestions.id')
+    table.string('date')
+    table.string('city')
+  })
+}
+
+export async function down(knex) {
+  await knex.schema.dropTable('travel_details')
+}
