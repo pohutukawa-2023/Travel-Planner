0460d1293dfc2dd9e291a6f829300e7679051dc1
Cong Guo <jeraviah@gmail.com>
2023-11-30 11:23:56 +1300

    Remove suggestion_id FK from travel_details table

--- server/db/migrations/20231129213841_travel_details.js
+++ server/db/migrations/20231129213841_travel_details.js
@@ -2,7 +2,6 @@ export async function up(knex) {
   await knex.schema.createTable('travel_details', (table) => {
     table.increments('id').primary()
     table.string('user_id').references('users.auth0_id')
-    table.integer('suggestion_id').references('suggestions.id')
     table.string('date')
     table.string('city')
   })
