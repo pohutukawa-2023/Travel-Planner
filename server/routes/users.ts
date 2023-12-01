62d6af3034cbd265dabcee16ee3269b6201b9086
Ricky <65267289+ricky-huang-dev@users.noreply.github.com>
2023-12-01 16:46:49 +1300

    rename auth0id

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -7,9 +7,9 @@ const router = express.Router()
 
 //POST /api/v1/users
 router.post('/', checkJwt, async (req: JwtRequest, res) => {
-  const auth0Id = req.auth?.sub
+  const auth0_id = req.auth?.sub
   const userDetails = req.body
-  const user = { ...userDetails, auth0Id }
+  const user = { ...userDetails, auth0_id }
   await db.upsertUser(user)
   res.sendStatus(201)
   return
