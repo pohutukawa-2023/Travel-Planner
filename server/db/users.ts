927fe1e0e80c6d5c10ed50f9309b9ceb7f78d7dd
Ricky <65267289+ricky-huang-dev@users.noreply.github.com>
2023-12-01 15:01:48 +1300

    create db function for user upsert

---
+++ server/db/users.ts
@@ -0,0 +1,14 @@
+import connection from './connection'
+import { User } from '../../models/ClientSuggestion'
+
+export async function upsertUser(user: User) {
+  await connection('users')
+    .insert({
+      auth0_id: user.auth0_id,
+      email: user.email,
+      first_name: user.first_name,
+      last_name: user.last_name,
+    })
+    .onConflict('auth0_id')
+    .merge()
+}
