9983c5fe2aa388200b9d486e7a2d18fb5324a2c0
Cong Guo <jeraviah@gmail.com>
2023-12-03 19:00:17 +1300

    Test out server routes

---
+++ server/routes/travel_details.test.ts
@@ -0,0 +1,99 @@
+import { vi, describe, it, expect } from 'vitest'
+import request from 'supertest'
+import server from '../server'
+import * as db from '../db/travel_details'
+import { getMockToken } from './mockToken'
+
+vi.mock('../db/travel_details')
+
+describe('POST /api/v1/travelDetail', () => {
+  it('should return 201 with an array', async () => {
+    const fakeResponse = [1]
+    vi.mocked(db.addTravelDetail).mockResolvedValue(fakeResponse)
+    const response = await request(server)
+      .post('/api/v1/travelDetail')
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(201)
+    expect(response.body).toEqual(fakeResponse)
+  })
+
+  it('should return 500 when no access token is passed', async () => {
+    vi.mocked(db.addTravelDetail).mockRejectedValue(new Error('test'))
+    const response = await request(server)
+      .post('/api/v1/travelDetail')
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({ message: 'Something wrong' })
+  })
+})
+
+describe('GET /api/v1/travelDetail', () => {
+  it('should return 200 with an array', async () => {
+    const fakeResponse = [
+      {
+        id: 1,
+        user_id: 'auth0|6569360d76c1b421367afd49',
+        date: '2028',
+        city: 'Wellington',
+      },
+    ]
+    vi.mocked(db.getTravelDetails).mockResolvedValue(fakeResponse)
+    const response = await request(server)
+      .get('/api/v1/travelDetail')
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(fakeResponse)
+  })
+
+  it('should return 500 when no access token is passed', async () => {
+    vi.mocked(db.getTravelDetails).mockRejectedValue(new Error('test'))
+    const response = await request(server)
+      .get('/api/v1/travelDetail')
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({ message: 'Something wrong' })
+  })
+})
+
+describe('GET /api/v1/travelDetail/:detailId', () => {
+  it('should return 200 with an object', async () => {
+    const fakeResponse = {
+      travelDetail: {
+        id: 1,
+        user_id: 'auth0|6569360d76c1b421367afd49',
+        date: '2028',
+        city: 'Wellington',
+      },
+      suggestions: [
+        {
+          id: 9,
+          name: 'Ortolana',
+          city_name: 'Auckland',
+          category: 'restaurant',
+          description:
+            'Enjoy a casual meal at Ortolana, offering fresh and seasonal produce.',
+          rating: 4.5,
+          address: '33 Tyler St, Britomart, Auckland',
+          opening_hours: '8 AM - 10 PM',
+          link: 'https://www.ortolana.co.nz/',
+          price_range: '15-30',
+        },
+      ],
+    }
+    vi.mocked(db.getTravelDetailAndSuggestions).mockResolvedValue(fakeResponse)
+    const response = await request(server)
+      .get('/api/v1/travelDetail/1')
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(fakeResponse)
+  })
+
+  it('should return 500 when no access token is passed', async () => {
+    vi.mocked(db.getTravelDetailAndSuggestions).mockRejectedValue(new Error('test'))
+    const response = await request(server)
+      .get('/api/v1/travelDetail/1')
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({ message: 'Something wrong' })
+  })
+})
