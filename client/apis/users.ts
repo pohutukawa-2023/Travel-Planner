5d0a603296e45938b1c611f822ae070ab1e5c627
ricky huang <rickyhetq12@gmail.com>
2023-12-03 16:05:51 +1300

    implement try catch to user routes

--- client/apis/users.ts
+++ client/apis/users.ts
@@ -1,9 +1,7 @@
 import request from 'superagent'
 import { User, UserDraft } from '../../models/ClientSuggestion'
 
+const baseUrl = '/api/v1/users'
 export async function upsertUser(user: User | UserDraft, token: string) {
-  await request
-    .post('/api/v1/users')
-    .set('Authorization', `Bearer ${token}`)
-    .send(user)
+  await request.post(baseUrl).set('Authorization', `Bearer ${token}`).send(user)
 }
--- server/routes/users.ts
+++ server/routes/users.ts
@@ -7,12 +7,16 @@ const router = express.Router()
 
 //POST /api/v1/users
 router.post('/', checkJwt, async (req: JwtRequest, res) => {
-  const auth0_id = req.auth?.sub
-  const userDetails = req.body
-  const user = { ...userDetails, auth0_id }
-  await db.upsertUser(user)
-  res.sendStatus(201)
-  return
+  try {
+    const auth0_id = req.auth?.sub as string
+    const userDetails = req.body
+    const user = { ...userDetails, auth0_id }
+    await db.upsertUser(user)
+    res.sendStatus(201)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'An error has occured' })
+  }
 })
 
 export default router
