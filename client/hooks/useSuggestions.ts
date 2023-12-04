fe2373bf93ac90d64a7d84a01355c10e1f63bcae
ricky huang <rickyhetq12@gmail.com>
2023-12-04 15:20:16 +1300

    pass token into suggestions hook

--- client/hooks/useSuggestions.ts
+++ client/hooks/useSuggestions.ts
@@ -1,13 +1,14 @@
 import { useQuery } from '@tanstack/react-query'
-
+import { useAuth0 } from '@auth0/auth0-react'
 import { getSuggestions } from '../apis/suggestions'
 
 function useSuggestions(city: string) {
+  const { getAccessTokenSilently } = useAuth0()
   const { data } = useQuery({
     queryKey: ['suggestion'],
     queryFn: async () => {
-      const response = await getSuggestions(city)
-      console.log(response)
+      const accessToken = await getAccessTokenSilently()
+      const response = await getSuggestions(city, accessToken)
       return response
     },
   })
